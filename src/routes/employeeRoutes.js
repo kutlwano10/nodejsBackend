103686f6a9f37e0df84d75dbb6ba8f5d8a0c529d
kutlwano10 <kutlwano.ramotebele@gmail.com>
2025-03-09 11:18:50 +0200

    removed :id

--- src/routes/employeeRoutes.js
+++ src/routes/employeeRoutes.js
@@ -8,6 +8,6 @@ router.get('/employee-hours', employeeHoursController.getAllEmployeeHours)
 router.delete('/hours/:id', employeeHoursController.deleteEmployeeHours)
 router.put('/hours/update/:id', employeeHoursController.updateEmployeeHours)
 router.post('/update/timesheet', employeeHoursController.updateEmployeeTimesheet)
-router.get('/retrieve/timesheet/:id', employeeHoursController.getEmployeeTimesheet)
+router.get('/retrieve/timesheet', employeeHoursController.getEmployeeTimesheet)
 
 module.exports = router
